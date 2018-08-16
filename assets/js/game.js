85bf64a910aecfcef98e7b3209c707c961b85788
Mike Reza <mikerza@gmail.com>
2018-08-15 22:38:58 -0500

    letters populate

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,16 +6,21 @@ const bank = ['Puffy Shirt','Bubble Boy','Shiksappeal','Sponge Worthy','Yada Yad
 let randomWord = Math.floor(Math.random() * bank.length);
 let chooseWord = bank[randomWord];
 let underScore = [];
+let rightWord=[];
+let wrongWord=[];
 
-//test with console
+//dom variable
+let domUnderScore = document.getElementsByClassName('inputDash');
+
+
+
+
 console.log(chooseWord);
-
-
 //Underscore matching
 let createUnderscore = () => {
     for(let i = 0; i < chooseWord.length; i++) {
         underScore.push('_');
-
+        
     }
     return underScore;
 }
@@ -24,14 +29,28 @@ console.log(createUnderscore());
 
 //player choice
 document.addEventListener('keypress', (event) => {
-    let convertKey = event.keyCode;
-    let keyConvert = String.fromCharCode(convertKey);
-//player is right
+    let keyConvert = String.fromCharCode(event.keyCode);
+    
+//player is right, add to the use words array
 
-    if (keyConvert.indexOf(chooseWord) > -1) {
-        console.log(true);
+    if (chooseWord.indexOf(keyConvert) > -1) {
+       rightWord.push(keyConvert);
+       underScore[chooseWord.indexOf(keyConvert)] = keyConvert;
+       domUnderScore[0].innerHTML = underScore.join(' ');
+       
+       
+       if (underScore.join(' ') == chooseWord) {
+           alert('Well, Well, Well. Looks like you know your stuff...');
+       }
+
     }
-
-
+    else { 
+        
+        wrongWrong.push(keyConvert);
+         
+       
+           
+    }
 });
 
+underScore[0].innerHTML = createUnderscore().join('_');
